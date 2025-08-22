58b8797e3c9c6e969fc5ad37287dc79c2f959c03
Juan Andres Trujillo <juan.trujillo1@protonmail.com>
2025-08-22 17:04:54 +0200

    fix search dropdown

--- app/page.tsx
+++ app/page.tsx
@@ -34,39 +34,50 @@ export default function HomePage() {
 	}
 
 	return (
-		<div>
-			<h1>The Infinite Library</h1>
-			<input
-				type='text'
-				value={searchQuery}
-				onChange={(e) => setSearchQuery(e.target.value)}
-				placeholder='Search author...'
-				onFocus={() => setShowDropdown(results.length > 0)}
-				onBlur={() => setTimeout(() => setShowDropdown(false), 100)}
-				onKeyDown={(e) => {
-					if (e.key === "Enter") {
-						const match = results.find(
-							(a) => a.name.toLowerCase() === searchQuery.toLowerCase()
-						);
-						if (match) {
-							handleSelect(match);
-						}
-					}
-				}}
-			/>
-			{showDropdown && (
-				<ul>
-					{results.map((author) => (
-						<li key={author.id} onMouseDown={() => handleSelect(author)}>
-							{author.name}
-						</li>
-					))}
-				</ul>
-			)}
-			<footer>
-				<a href='https://github.com/juanandrestt/infinite-library'>github</a>
+		<>
+			<main className='relative flex flex-col items-center justify-center min-h-screen'>
+				<h1 className='absolute top-6 text-4xl font-bold'>
+					The Infinite Library
+				</h1>
+
+				<div className='relative w-100'>
+					<input
+						className='w-full border border-gray-300 p-2 focus:outline-none'
+						type='text'
+						value={searchQuery}
+						onChange={(e) => setSearchQuery(e.target.value)}
+						placeholder='Search author...'
+						onFocus={() => setShowDropdown(results.length > 0)}
+						onBlur={() => setTimeout(() => setShowDropdown(false), 100)}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								const match = results.find(
+									(a) => a.name.toLowerCase() === searchQuery.toLowerCase()
+								);
+								if (match) {
+									handleSelect(match);
+								}
+							}
+						}}
+					/>
+					{showDropdown && (
+						<ul className='absolute top-full left-0 right-0 mt-1 z-50'>
+							{results.map((author) => (
+								<li
+									key={author.id}
+									onMouseDown={() => handleSelect(author)}
+									className='p-2 hover:bg-gray-100 cursor-pointer'>
+									{author.name}
+								</li>
+							))}
+						</ul>
+					)}
+				</div>
+			</main>
+			<footer className='absolute bottom-4 right-8 flex flex-col'>
+				<a href='https://github.com/juanandrestt/marginalia'>github</a>
 				<a href='https://www.juantrujillo.world'>by Juan Trujillo</a>
 			</footer>
-		</div>
+		</>
 	);
 }
